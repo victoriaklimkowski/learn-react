8a3eceb4d2e21222b47bcde2ae262504c12ecfdf
victoriaklimkowski <victoriablairklimkowski@gmail.com>
2024-10-03 15:02:55 -0400

    setup index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
-import List from "./qcomps/list_keys_id";
-import RecipeList from "./qcomps/recipes";
-import Gallery from "./qcomps/state";
-import Form from "./qcomps/stuckForm";
-import FeedbackForm from "./qcomps/thankYouCrash";
 
 export default function Home() {
   return (
     <>
-      {/* React Lists and States Tutorial Lab  */}
-      <FeedbackForm />
-      <Form />  
-      <Gallery />
-      <RecipeList />
-      <List />
+      {/* React Update State Tutorial Lab  */}
+
     </>
   )
 }
