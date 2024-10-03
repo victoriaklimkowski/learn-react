c7a774d073abf89f3f090ff9b153fa444fb6e438
Victoria Klimkowski <122411763+victoriaklimkowski@users.noreply.github.com>
2024-10-03 12:43:58 -0400

    updated index only to setup for React Lists and State lab

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import Profile from "./qcomps/profile_mistake";
-import MyComp from "./qcomps/firstcomp";
-import Bio from "./qcomps/bios";
-import Gallery from "./qcomps/gallery_props";
-import PackingList from "./qcomps/props_item";
+import List from "./qcomps/list_keys_id";
+import RecipeList from "./qcomps/recipes";
+import Gallery from "./qcomps/state";
+import Form from "./qcomps/stuckForm";
+import FeedbackForm from "./qcomps/thankYouCrash";
 
 export default function Home() {
   return (
     <>
-      <Bio />
-      <MyComp />
-      <Profile />
+      <FeedbackForm />
+      <Form />  
       <Gallery />
-      <PackingList />
+      <RecipeList />
+      <List />
     </>
   )
 }
